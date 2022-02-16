fe1dad4612b0c9f6f2a9a523a61df173ed00e422
Herve Leclerc <herve.leclerc@alterway.fr>
2022-02-16 08:58:28 +0100

    my challeng

---
+++ challenge-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+function solver() {
+  SOLVER_IMAGE=ghcr.io/javajon/solver:0.5.3   ## <-- Set to the latest semver from https://github.com/javajon/katacoda-solver/releases]
+  SCENARIOS_ROOT=~/dev/projets/perso/katacoda/herveleclerc-katacoda               ## <-- Set to your base source directory for your challenges and scenarios
+  if [[ ! "$(pwd)" =~ ^$SCENARIOS_ROOT.* ]]; then
+    echo "Please run this from $SCENARIOS_ROOT or one of its scenario or challenge subdirectory."
+    return 1;
+  fi
+  SUBDIR=$(echo $(pwd) | grep -oP "^$SCENARIOS_ROOT\K.*")
+  docker run --rm -it -v "$SCENARIOS_ROOT":/workdir -w /workdir/$SUBDIR $SOLVER_IMAGE "$@";
+}
