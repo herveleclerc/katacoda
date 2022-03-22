1dc199cbb62d36990ad7b7ed91c35fa2cc3d3205
Herve Leclerc <herve.leclerc@alterway.fr>
2022-03-22 15:05:53 +0100

    recheck giveup

--- cka-challenges/cka-002/assets/verifications.sh
+++ cka-challenges/cka-002/assets/verifications.sh
@@ -9,7 +9,7 @@
 export HINTS_DELAY=15
 
 # Max time for verify blocking commands. Zero or positive integer time units s, m, h.
-export VERIFY_TIMEOUT=3s
+export VERIFY_TIMEOUT=10s
 
 export ETCDCTL_API=3
 
@@ -17,7 +17,27 @@ export kctl="/usr/bin/kubectl --kubeconfig=/root/.kube/config"
 
 export ectl="etcdctl --endpoints=https://$(hostname -i | awk '{print $2;}'):2379 --cacert="/etc/kubernetes/pki/etcd/ca.crt" --cert=/etc/kubernetes/pki/etcd/healthcheck-client.crt --key=/etc/kubernetes/pki/etcd/healthcheck-client.key"
 
+
+function write_progress() {
+  echo -n "$1" >> /opt/.logs/progress.txt
+  return 0
+}
+
+function check_give_up() {
+  if [[ -f /opt/.logs/give_up ]]; then
+    rm -f /opt/.logs/give_up
+    write_progress "Task $1: KO;"
+    return 1
+  fi
+  return 0
+}
+
 function verify_task_1() {
+
+  check_give_up 1; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   if [ -f "/tmp/etcd-backup.db" ]
   then
@@ -25,6 +45,7 @@ function verify_task_1() {
     if [[ "$content" == "1" ]]
     then
       echo "Verification passed"
+       write_progress "Task 1: OK;"
       return 0
     else
       echo "Verification failed"
@@ -37,6 +58,11 @@ function verify_task_1() {
 }
 
 function verify_task_2() {
+
+  check_give_up 2; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   content=$(${kctl} get pods -n default --no-headers --selector run=elliphant | grep elliphant | awk '{print $3;}')
   request_cpu=$(${kctl} get -n default -o jsonpath='{.spec.containers[0].resources.requests.cpu}' po elliphant)
@@ -49,6 +75,7 @@ function verify_task_2() {
       if [[ "$request_memory" == "200Mi" ]]
       then
         echo "Verification passed"
+        write_progress "Task 2: OK;"
         return 0
       else
         echo "Verification failed"
@@ -66,6 +93,11 @@ function verify_task_2() {
 
 
 function verify_task_3() {
+
+  check_give_up 3; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   content=$(${kctl} get pods -n default --no-headers --selector run=super-user-pod | grep super-user-pod | awk '{print $3;}')
   capabilities=$(${kctl} get -n default -o jsonpath='{.spec.containers[0].securityContext.capabilities.add[0]}' po super-user-pod)
@@ -76,6 +108,7 @@ function verify_task_3() {
       if [[ "$capabilities" == "SYS_TIME" ]]
       then
         echo "Verification passed"
+        write_progress "Task 3: OK;"
         return 0
       else
         echo "Verification failed"
@@ -88,6 +121,11 @@ function verify_task_3() {
 }
 
 function verify_task_4() {
+
+  check_give_up 4; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   content=$(${kctl} get pods -n default --no-headers --selector run=use-pv | grep use-pv | awk '{print $3;}')
 
@@ -103,7 +141,11 @@ function verify_task_4() {
       if [[ "$mountpath" == "/data" ]]
       then
         echo "Verification passed"
+        write_progress "Task 4: OK;"
         return 0
+      else
+        echo "Verification failed"
+        return 1
       fi
      done
   else
@@ -113,6 +155,11 @@ function verify_task_4() {
 }
 
 function verify_task_5() {
+
+  check_give_up 5; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   content=$(${kctl} get pods -n default --no-headers --selector app=nginx-deploy | grep nginx-deploy | awk '{print $3;}')
   
@@ -124,6 +171,7 @@ function verify_task_5() {
       if [[ "$annotation" == "2" ]]
       then
         echo "Verification passed"
+        write_progress "Task 5: OK;"
         return 0
       else
         echo "Verification failed"
@@ -137,6 +185,11 @@ function verify_task_5() {
 
 
 function verify_task_6() {
+
+  check_give_up 6; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   create=$(${kctl} auth can-i create pods --as=alterway --namespace=development)
   watch=$(${kctl} auth can-i watch pods --as=alterway --namespace=development)
@@ -147,6 +200,7 @@ function verify_task_6() {
       if [[ "$watch" == "no" ]]
       then
         echo "Verification passed"
+        write_progress "Task 6 OK;"
         return 0
       else
         echo "Verification failed"
@@ -159,6 +213,11 @@ function verify_task_6() {
 }
 
 function verify_task_7() {
+
+  check_give_up 7; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   hostname=$(hostname)
 
@@ -178,11 +237,17 @@ function verify_task_7() {
     return 1
   else
     echo "Verification passed"
+    write_progress "Task 7: OK;"
     return 0
   fi
 }
 
 function verify_task_8() {
+
+  check_give_up 8; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
 
   if [[ ! -f "/tmp/pod-over-request.txt" ]]
   then
@@ -195,6 +260,7 @@ function verify_task_8() {
   if [[ "$c" == "1" ]]
   then
     echo "Verification passed"
+    write_progress "Task 8: OK;"
     return 0
   else
     echo "Verification failed"
@@ -203,6 +269,11 @@ function verify_task_8() {
 }
 
 function verify_task_9() {
+
+  check_give_up 9; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
  if [[ ! -f "/tmp/healthchecking.txt" ]]
   then
@@ -215,6 +286,7 @@ function verify_task_9() {
   if [[ "$c" == "1" ]]
   then
     echo "Verification passed"
+    write_progress "Task 9: OK;"
     return 0
   else
     echo "Verification failed"
@@ -225,6 +297,10 @@ function verify_task_9() {
 
 function verify_task_10() {
 
+  check_give_up 10; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
 
   c=$(${kctl} get po -n constraints -o wide | grep app001 | grep -c node01)
  
@@ -235,11 +311,17 @@ function verify_task_10() {
     return 1
   else
     echo "Verification passed"
+    write_progress "Task 10: OK;"
     return 0
   fi
 }
 
 function verify_task_11() {
+
+  check_give_up 11; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   if [[ ! -f "/tmp/supersecret.txt" ]]
   then
@@ -255,11 +337,17 @@ function verify_task_11() {
     return 1
   else
     echo "Verification passed"
+    write_progress "Task 11: OK;"
     return 0
   fi
 }
 
 function verify_task_12() {
+
+  check_give_up 12; ec=$?
+  if [[ $ec -eq 1 ]]; then
+    return 0
+  fi
  
   if [[ ! -f "/tmp/mystery.txt" ]]
   then
@@ -274,6 +362,7 @@ function verify_task_12() {
     return 1
   else
     echo "Verification passed"
+    write_progress "Task 12: OK;"
     return 0
   fi
   
@@ -290,13 +379,27 @@ function verify_task_13() {
        echo "Verification failed"
        return 1
      else
+
+       if [[ -f /opt/.logs/progress.txt ]]; then
+         c=$(grep -c "KO" /opt/.logs/progress.txt)
+          if [[ $c -ne 0 ]]; then
+             titre="Réussite partielle Challenge cka-002 (katacoda)"
+             text="$prenom $nom ($email) a réussi partiellement la certification"
+          else
+             titre="Réussite du Challenge cka-002 (katacoda)"
+             text="$prenom $nom ($email) a réussi la certification"
+          fi
+       else
+          title="Erreur"
+          text="Erreur"
+       fi
        if [[ "$prenom" != "CHANGEZ-MOI" && "$nom" != "CHANGEZ-MOI" && "$code" != "CHANGEZ-MOI"  && "$email" != "CHANGEZ-MOI" ]]; then
           curl --fail -X POST -H 'Content-Type: application/json' \
             --data "{\"alias\":\"strongmind\",
             \"emoji\":\":strongmind:\",
             \"text\":\"Challenge cka-002\",
-            \"attachments\":[{\"title\":\"Réussite du Challenge cka-002 (katacoda)\",
-            \"title_link\":\"https://www.katacoda.com/awh/courses/cka-challenges/cka-002\",\"text\":\"$prenom $nom ($email) a réussi la certification\",\"color\":\"#764FA5\"}]}" \
+            \"attachments\":[{\"title\":\"$title\",
+            \"title_link\":\"https://www.katacoda.com/awh/courses/cka-challenges/cka-002\",\"text\":\"$text\",\"color\":\"#764FA5\"}]}" \
             https://rocket.alterway.fr/hooks/QxnH7sqdQwLeby5vP/E5KwEWg88cT8RHrJJifYzdG5wfL5RPySPzKwMxZp$code
             retVal=$?
             if [ $retVal -ne 0 ]
